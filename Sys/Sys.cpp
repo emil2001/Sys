61c832e478b5b6d0fd93a6cbdbdf96aefe3d37ee
Emlg_laptop <emilek2001@yandex.ru>
2019-12-03 10:37:39 +0300

    sys

--- Sys/Sys.cpp
+++ Sys/Sys.cpp
@@ -1,26 +1,18 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <locale.h>
 #include <math.h>
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-
 	int n, m, res = 0;
 	int k = 1;
-
-	printf("Введите число: ");
-	scanf("%d", &n);
-	printf("Перевести в: ");
-	scanf("%d", &m);
+	scanf("%d%d", &n, &m);
 	int mm = 1;
 	for (int nn = n; nn >= m; nn /= m)
 	{
 		mm *= m;
 	}
 
-	printf("Ответ: ");
 	while (mm != 0)
 	{
 		int d = n / mm;
